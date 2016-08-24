96ad0b95eece61d946e06efd7c97bb8af962a047
Tzyy Tong <tzyy.tong@outlook.com>
2016-08-24 08:39:44 +0100

    Added Python files for statistics_and_machinelearning.py

---
+++ HackerRank.Python/artificial_intelligence/probability_and_statistics.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 23 22:31:21 2016
+
+@author: TzyyShyang
+"""
+
+#!/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 23 21:07:35 2016
+
+@author: TzyyShyang
+"""
+
+import sys
+import numpy as np
+
+a0,a1,a2 = input().strip().split(' ')
+a0,a1,a2 = [int(a0),int(a1),int(a2)]
+b0,b1,b2 = input().strip().split(' ')
+b0,b1,b2 = [int(b0),int(b1),int(b2)]
